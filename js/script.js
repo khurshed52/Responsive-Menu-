43c797eaade3f2801c51211af8b50ad4e9547b80
Khurshed khan <khurshedkhan52@gmail.com>
2017-09-30 22:31:29 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+(function($) {
+	
+	"use strict";
+	
+	//Update header style + Scroll to Top
+	function headerStyle() {
+		if($('.main-header').length){
+			var windowpos = $(window).scrollTop();
+			if (windowpos >= 250) {
+				$('.main-header').addClass('fixed-header');
+				$('.scroll-to-top').fadeIn(300);
+			} else {
+				$('.main-header').removeClass('fixed-header');
+				$('.scroll-to-top').fadeOut(300);
+			}
+		}
+	}
+	
+	headerStyle();
+	
+	
+	//Submenu Dropdown Toggle
+	if($('li.dropdown ul').length){
+		$('li.dropdown').append('<div class="dropdown-btn"><span class="fa fa-angle-down"></span></div>');
+		
+		//Dropdown Button
+		$('li.dropdown .dropdown-btn').on('click', function() {
+			$(this).prev('ul').slideToggle(500);
+		});
+		
+		
+		//Disable dropdown parent link
+		$('.navigation li.dropdown > a').on('click', function(e) {
+			e.preventDefault();
+		});
+	}
+	
+	
+
+})(window.jQuery);
